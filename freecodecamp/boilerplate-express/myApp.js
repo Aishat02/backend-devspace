ba2619b0fabba7564c088832eb894c13a3bd9e31
Aris <titilayotijani15@gmail.com>
2025-10-27 15:39:17 +0100

    reorder middleware

--- freecodecamp/boilerplate-express/myApp.js
+++ freecodecamp/boilerplate-express/myApp.js
@@ -1,11 +1,13 @@
 let express = require("express");
 let app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
+
 let absolutePath = __dirname + "/views/index.html";
 
-app.get("/", function (req, res) {
+app
+  .get("/", function (req, res) {
     res.sendFile(absolutePath);
   })
   .listen(8080, () => console.log("Server is running"));
 
-app.use("/public", express.static(__dirname + "/public"));
