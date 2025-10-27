32933ceb960150c6c838ccb86b1d4e2b913425b3
Aris <titilayotijani15@gmail.com>
2025-10-27 14:24:57 +0100

    serve static asset

--- freecodecamp/boilerplate-express/myApp.js
+++ freecodecamp/boilerplate-express/myApp.js
@@ -7,3 +7,5 @@ app.get("/", function (req, res) {
     res.sendFile(absolutePath);
   })
   .listen(8080, () => console.log("Server is running"));
+
+app.use("/public", express.static(__dirname + "/public"));
