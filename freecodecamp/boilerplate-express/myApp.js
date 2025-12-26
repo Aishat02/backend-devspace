3a90a5739bf5a0e37c7210c9d5f363367c84e306
Aris <titilayotijani15@gmail.com>
2025-12-26 13:39:57 +0100

    add middleware and environment config

--- freecodecamp/boilerplate-express/myApp.js
+++ freecodecamp/boilerplate-express/myApp.js
@@ -1,6 +1,11 @@
+let dotenv = require("dotenv").config();
 let express = require("express");
 let app = express();
 
+app.use("/", (req, res, next) => {
+  console.log(`${req.method}${req.path} - ${req.ip}`);
+  next();
+});
 app.use("/public", express.static(__dirname + "/public"));
 
 let absolutePath = __dirname + "/views/index.html";
@@ -10,5 +15,13 @@ app
     res.sendFile(absolutePath);
   })
   .listen(8080, () => console.log("Server is running"));
+
+app.get("/json", function (req, res) {
+  let text = { message: "Hello json" };
+
+  process.env.MESSAGE_STYLE === "uppercase"
+    ? res.json(text.message.toUpperCase())
+    : res.json(text.message.toLowerCase());
+});
 
 module.exports = app;
